293600a0ba489670388f7861dfa48d01beb2105e
laitou <2395355749@qq.com>
2018-12-02 17:58:15 +0800

    menu

---
+++ src/App.vue
@@ -0,0 +1,76 @@
+<template>
+  <div id="app">
+    <el-container>
+      <el-header style="padding: 0px">
+        <div class="head-name">sjjd</div>
+        <div>sss</div>
+      </el-header>
+      <el-container>
+        <el-aside width="200px">
+          <pp-nav/>
+        </el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+import { Container, Header, Aside, Main, Row, Col } from "element-ui";
+import Nav from "./components/Nav";
+export default {
+  name: 'app',
+  data: function(){
+    return {
+      visible: false
+    }
+  },
+  components: {
+    'el-container': Container,
+    'el-header': Header,
+    'el-aside': Aside,
+    'el-main': Main,
+    'el-row': Row,
+    'el-col': Row,
+    'pp-nav': Nav
+  }
+}
+</script>
+
+<style>
+body{
+  margin: 0;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+ .el-header {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+  .head-name{
+    width: 200px;
+    border-right: 1px solid #fff;
+  }
+  
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    min-height: 200px;
+  }
+  
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    min-height: calc(100vh - 60px);
+  }
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,63 @@
+<template>
+    <el-menu
+      default-active="2"
+      class="el-menu-vertical-demo"
+      @open="handleOpen"
+      @close="handleClose"
+      >
+      <el-submenu v-for="(m, i) in menus"
+        :key="i"
+        v-bind:index="String(i)">
+          <template slot="title">{{m.name}}</template>
+          <el-menu-item v-for="(s, j) in m.children" 
+            v-if="m.children.length > 0"
+            :key="j"
+           v-bind:index="String(i)+'-'+String(j)">{{s.name}}</el-menu-item>
+        </el-submenu>
+    </el-menu>
+</template>
+<style>
+
+</style>
+
+<script>
+import { Menu, MenuItem, MenuItemGroup } from "element-ui";
+export default {
+    name: "pp-nav",
+     methods: {
+      handleOpen(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      handleClose(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    },
+    data: function(){
+            return {
+                    menus: [{
+                    name: "数据",
+                    icon: "el-icon-location",
+                    children: [{
+                        name: "Table"
+                    },{
+                        name: "总览"
+                    }]
+                },{
+                    name: "数据",
+                    icon: "el-icon-location",
+                    children: [{
+                        name: "Table"
+                    },{
+                        name: "总览"
+                    }]
+                }]
+            } 
+    },
+    components: {
+        "el-menu": Menu,
+        "el-menu-item": MenuItem,
+        "el-menu-item-group": MenuItemGroup
+    }
+}
+</script>
+
